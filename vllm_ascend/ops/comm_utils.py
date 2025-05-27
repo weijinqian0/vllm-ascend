62fa6ad083b1340fa38056c2f5b804cb6edd2bc8
weijinqian_v1 <weijinqian@huawei.com>
2025-05-27 14:22:58 +0800

    bug fixed: add moe dispatcher

--- vllm_ascend/ops/comm_utils.py
+++ vllm_ascend/ops/comm_utils.py
@@ -12,7 +12,7 @@ def async_all_gather(input_, group, event=None, is_use_get_global_memory_buffer=
     new_dim_size = dim_size[0] * world_size
     dim_size[0] = new_dim_size
 
-    ag_out = torch.empty(dim_size, dtype=input_.dtype, device=torch.cuda.current_device())
+    ag_out = torch.empty(dim_size, dtype=input_.dtype, device=torch.npu.current_device())
     if event:
         # multi stream wait event
         global COMM_STREAM
@@ -35,7 +35,7 @@ def async_reduce_scatter(input_, group, event=None, stream=None, is_use_get_glob
     dim_size = list(input_.size())
     dim_size[0] = dim_size[0] // world_size
 
-    rs_out = torch.empty(dim_size, dtype=input_.dtype, device=torch.cuda.current_device())
+    rs_out = torch.empty(dim_size, dtype=input_.dtype, device=torch.npu.current_device())
     if event or stream:
         # multi stream wait event
         global COMM_STREAM
@@ -45,7 +45,7 @@ def async_reduce_scatter(input_, group, event=None, stream=None, is_use_get_glob
             if event:
                 event.wait()
             if stream:
-                torch.cuda.current_stream().wait_stream(stream)
+                torch.npu.current_stream().wait_stream(stream)
             handle = torch.distributed.reduce_scatter_tensor(
                 rs_out, input_.contiguous(), group=group, async_op=True
             )
@@ -65,7 +65,7 @@ def async_all_to_all(input_, output_split_sizes, input_split_sizes, group, event
         a2a_out = input_.new_empty(
             size=[sum(output_split_sizes)] + list(input_.size()[1:]),
             dtype=input_.dtype,
-            device=torch.cuda.current_device(),
+            device=torch.npu.current_device(),
         )
 
     if event:
