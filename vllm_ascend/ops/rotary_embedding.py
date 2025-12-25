35d4c8928c52d4ed9430f458923f3e9297accb40
weijinqian_v1 <weijinqian@huawei.com>
2025-12-25 13:27:58 +0800

    [Refactor] use cos_sin_cache & remove parameter like model in builder.
    Signed-off-by: weijinqian_v1 <weijinqian@huawei.com>

--- vllm_ascend/ops/rotary_embedding.py
+++ vllm_ascend/ops/rotary_embedding.py
@@ -63,7 +63,6 @@ def set_cos_and_sin(vllm_config, max_num_reqs, decode_token_per_req, dtype,
         _sin is not None:
         return
 
-    compilation_config = vllm_config.compilation_config
     model_config = vllm_config.model_config
     max_num_batched_tokens = vllm_config.scheduler_config.max_num_batched_tokens
 
